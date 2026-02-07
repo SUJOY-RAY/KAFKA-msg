4ad96559a01f12acf31c4961ab73f5ff4752be51
SUJOY RAY <sujoygithub@gmail.com>
2026-02-08 00:20:17 +0530

    .

---
+++ src/main/java/com/messaging/kafkamsg/KafkamsgApplication.java
@@ -0,0 +1,13 @@
+package com.messaging.kafkamsg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkamsgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkamsgApplication.class, args);
+	}
+
+}
