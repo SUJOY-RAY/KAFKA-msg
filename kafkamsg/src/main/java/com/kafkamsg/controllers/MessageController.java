b7acb8bbaafcc7ab06d6770ec0282c5101f0c1bb
SUJOY RAY <sujoygithub@gmail.com>
2026-02-08 18:33:44 +0530

    consumer added

--- kafkamsg/src/main/java/com/kafkamsg/controllers/MessageController.java
+++ kafkamsg/src/main/java/com/kafkamsg/controllers/MessageController.java
@@ -3,26 +3,41 @@ package com.kafkamsg.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kafkamsg.services.MessageConsumer;
 import com.kafkamsg.services.MessageProducer;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
 @RequestMapping("/messages")
 public class MessageController {
     private final MessageProducer producer;
+    private final MessageConsumer consumer;
 
-    public MessageController(MessageProducer producer) {
+    public MessageController(MessageProducer producer, MessageConsumer consumer) {
         this.producer = producer;
+        this.consumer = consumer;
     }
-    
+
     @PostMapping
     public ResponseEntity<String> send(@RequestBody String message) {
         producer.send(message);
         return ResponseEntity.ok("Message Sent to Kafka.");
     }
+
+    @GetMapping("/consumed")
+    public List<String> consumed() {
+        return consumer.getMessages();
+    }
+    
     
 }
--- kafkamsg/src/main/java/com/kafkamsg/services/MessageConsumer.java
+++ kafkamsg/src/main/java/com/kafkamsg/services/MessageConsumer.java
@@ -1,12 +1,22 @@
 package com.kafkamsg.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MessageConsumer {
+    private final List<String> messages = new ArrayList<>();
+    
     @KafkaListener(topics = "messages", groupId = "message-group")
     public void consume(String message) {
+        messages.add(message);
         System.out.println("Consumed message: " + message);
+    }
+
+    public List<String> getMessages() {
+        return messages;
     }
 }
